a277c828f350b0e6496a8321ae040509db96a5a7
Anonymous642 <Anonymous642@fake.email>
2024-11-19 17:35:04 -0500

    git is annoying

---
+++ prv/Secure/Core/Src/prefix.c
@@ -0,0 +1,37 @@
+/*
+ * prefix.c
+ *
+ * Created on: Oct 7, 2024
+ */
+
+#include "prefix.h"
+#include "speculation.h"
+#include "cfa_engine.h"
+
+uint32_t prefix_mask;
+uint32_t prefix;
+
+void prefix_reset(){
+	prefix = 0;
+	prefix_mask = 0;
+	for(int i=0; i<PREFIX_BIT_LEN; i++){
+		prefix_mask = 0x80000000 | (prefix_mask >> 1);
+	}
+}
+
+int prefix_handler(uint32_t write_value, CFA_REPORT * report_secure, uint8_t * to_log){
+	int size = SUFFIX_BYTE_LEN;
+
+	if ((to_log[3] == REPEAT_SYMBOL) && (to_log[2]== REPEAT_SYMBOL)){
+		size = 3;
+	}else if ((to_log[3] == SPEC_ID_SYMBOL) && (to_log[2] == SPEC_ID_SYMBOL) && (to_log[1] == SPEC_ID_SYMBOL)){
+		size = 2;
+	}else if ((write_value & prefix_mask) != prefix){
+		prefix = (write_value & prefix_mask);
+		for(int i=4; i<4+SUFFIX_BYTE_LEN; i++){
+			to_log[i] = 0x33;
+		}
+		size = size + 4;
+	}
+	return size;
+}
